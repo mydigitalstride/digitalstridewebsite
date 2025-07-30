a666940be206450fa1a65b3a9ef79cd7221bbb86
Rianna Barrett <riannabarrett11@gmail.com>
2025-07-30 14:55:39 -0400

    add link to we also do section

--- template-parts/section-we-also-do.php
+++ template-parts/section-we-also-do.php
@@ -5,13 +5,19 @@
       <?php if (have_rows('we_also_do_item')): ?>
         <?php while (have_rows('we_also_do_item')): the_row(); 
           $item_text = get_sub_field('item');
-          $icon = get_sub_field('icon'); // image array
+          $icon = get_sub_field('icon');
+          $link = get_sub_field('link_url'); 
         ?>
           <div class="we-also-do-item">
             <?php if ($icon): ?>
               <img class="we-also-do-icon" src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($item_text); ?>">
             <?php endif; ?>
-            <p><?php echo esc_html($item_text); ?></p>
+
+            <?php if ($link): ?>
+              <p><a class="we-also-do-link" href="<?php echo esc_url($link); ?>"><?php echo esc_html($item_text); ?></a></p>
+            <?php else: ?>
+              <p><?php echo esc_html($item_text); ?></p>
+            <?php endif; ?>
           </div>
         <?php endwhile; ?>
       <?php endif; ?>
